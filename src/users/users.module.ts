12c1d03dfd362292c55c7a62a84ef6d5a3890b57
Biplob <biplob.ruet15@gmail.com>
2022-09-24 16:38:56 +0600

    create user entity and connect it to user & app module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [User],
 })
 export class UsersModule {}
