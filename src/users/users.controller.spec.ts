b156b6c5b557c39d50f1487a77f38480b1e5ae0e
Biplob <biplob.ruet15@gmail.com>
2022-11-21 18:23:15 +0600

    set up with fake dependency implementation for user controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUserService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeAuthService = {
+      // signin: () => {},
+      // signUp: () => {}
+    };
+    fakeUserService = {
+      find: (email: string) =>
+        Promise.resolve([{ id: 1, email, password: 'haha' } as User]),
+      findOne: (id: number) =>
+        Promise.resolve({ id, email: 'ha@ha', password: 'haha' } as User),
+      // update: () => {},
+      // remove: () => {}
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: UsersService, useValue: fakeUserService },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
+  it('should be defined ok', () => {
     expect(controller).toBeDefined();
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,7 +18,7 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Serialize(UserDto)
 @Controller('auth')
