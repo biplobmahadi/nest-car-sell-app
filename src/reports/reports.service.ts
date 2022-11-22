ea714b04845bea0719c6e44d3cba71b78e02a029
Biplob <biplob.ruet15@gmail.com>
2022-11-22 23:17:08 +0600

    create a post req to create report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  create(reportDto: CreateUserDto) {
+    return 'ok';
+  }
+}
