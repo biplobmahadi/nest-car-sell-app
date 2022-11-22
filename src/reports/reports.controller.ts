4646efc9cae79c63a86cc9e3cb3806a1891a6a97
Biplob <biplob.ruet15@gmail.com>
2022-11-23 00:37:53 +0600

    serialize report res with userId

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReports(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
