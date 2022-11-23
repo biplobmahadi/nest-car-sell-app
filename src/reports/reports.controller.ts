1d1fa02a703d0dd5e0c84f6457af38a2769942ce
Biplob <biplob.ruet15@gmail.com>
2022-11-23 17:06:36 +0600

    add a route to get car estimate price

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   Patch,
   Post,
   UseGuards,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
 import { ReportsService } from './reports.service';
@@ -15,10 +17,16 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.estimate(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
